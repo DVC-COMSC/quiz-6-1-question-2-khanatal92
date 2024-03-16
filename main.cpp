557ef206570981f53efd7ac7f515d612457bc202
khanatal92 <akhan126@insite.4cd.edu>
2024-03-15 19:45:05 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -18,6 +18,45 @@ int writeFile(const string& filename) {
         return -1;
     }
 
+for (int i = 0; i < numEmployees; ++i) {
+        int id;
+        string name, department;
+        double salary;
+
+        cout << "Enter employee ID, name, department, and salary (separated by spaces): ";
+        cin >> id >> name >> department >> salary;
+        cin.ignore(); // Ignore the newline character in the input buffer
+
+        file << id << " " << name << " " << department << " " << salary << endl;
+    }
+
+    file.close();
+    return numEmployees;
+}
+
+// Function to read and print employee information from file
+int readFile(const string& filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Unable to open file." << endl;
+        return -1;
+    }
+
+    int numEmployees = 0;
+    int id;
+    string name, department;
+    double salary;
+
+    cout << "Employee information in file:" << endl;
+    while (file >> id >> name >> department >> salary) {
+        cout << id << " " << name << " " << department << " " << salary << endl;
+        ++numEmployees;
+    }
+
+    file.close();
+    return numEmployees;
+}
+
 int main()
 {
 
