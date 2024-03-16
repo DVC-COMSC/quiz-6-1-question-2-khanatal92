67f91c66b6cbb9bc9f8ea764be8cf85fd2859777
khanatal92 <akhan126@insite.4cd.edu>
2024-03-15 19:40:03 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "main.hpp"
+
+using namespace std;
+
+// Function to write employee information to file
+int writeFile(const string& filename) {
+    int numEmployees;
+    cout << "Enter the number of employees: ";
+    cin >> numEmployees;
+    cin.ignore(); // Ignore the newline character in the input buffer
+
+    ofstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Unable to open file." << endl;
+        return -1;
+    }
 
 int main()
 {
@@ -12,4 +30,6 @@ int main()
 
     empN = readFile(filename);
     cout << "The total number of employess read " << empN << endl;
+
+    return 0;
 }
