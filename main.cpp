f8c5476c2e0c2c6356df179a9e0813b5e4652db8
khanatal92 <akhan126@insite.4cd.edu>
2024-03-15 20:04:06 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@
 
 using namespace std;
 
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+#include "main.hpp"
+
+using namespace std;
+
 int writeFile() {
     ofstream ofs("employee.txt");
-    if (!ofs.is_open()) {
-        cerr << "Error opening file." << endl;
+    if (!ofs) {
+        cerr << "File Open Error" << endl;
         return -1;
     }
 
@@ -24,51 +32,46 @@ int writeFile() {
 
         cout << "Enter the Employee ID, Name, Department, and Salary for employee " << (i + 1) << ": ";
         cin >> employeeID >> employeeName >> departmentName >> salary;
-
         ofs << employeeID << " " << employeeName << " " << departmentName << " " << salary << endl;
     }
 
     ofs.close();
+    cout << "Employee information has been written to employee.txt" << endl;
     return numEmployees;
 }
 
 int readFile() {
     ifstream ifs("employee.txt");
-    if (!ifs.is_open()) {
-        cerr << "Error opening file." << endl;
+    if (!ifs) {
+        cerr << "File Open Error" << endl;
         return -1;
     }
 
     int numEmployees;
     ifs >> numEmployees;
-    cout << "Number of employees: " << numEmployees << endl;
+    cout << setw(12) << "ID" << setw(20) << "Name" << setw(20) << "Department" << setw(12) << "Salary" << endl;
+
+    int employeeID;
+    string employeeName, departmentName;
+    double salary;
+    double totalSalary = 0;
 
     for (int i = 0; i < numEmployees; ++i) {
-        int employeeID;
-        string employeeName, departmentName;
-        double salary;
-
         ifs >> employeeID >> employeeName >> departmentName >> salary;
-        cout << employeeID << " " << employeeName << " " << departmentName << " " << salary << endl;
+
+        cout << setw(12) << employeeID << setw(20) << employeeName << setw(20) << departmentName << setw(12) << salary << endl;
+
+        totalSalary += salary;
     }
 
+    cout << setw(52) << "Total: " << setw(12) << totalSalary << " Average: " << setw(12) << fixed << setprecision(2) << (totalSalary / numEmployees) << endl;
+
     ifs.close();
-    return numEmployees;
+    return 0;
 }
 
 int main() {
-    int numEmployeesWritten = writeFile();
-    if (numEmployeesWritten > 0) {
-        cout << "Employee information has been written to employee.txt." << endl;
-        int numEmployeesRead = readFile();
-        if (numEmployeesRead > 0) {
-            cout << "Total number of employees read: " << numEmployeesRead << endl;
-        } else {
-            cerr << "Error reading employee information from file." << endl;
-        }
-    } else {
-        cerr << "Error writing employee information to file." << endl;
-    }
-
+    writeFile();
+    readFile();
     return 0;
 }
