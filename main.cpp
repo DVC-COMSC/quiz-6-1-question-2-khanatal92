93fdad11befb37559b0e9273770397931d3bf623
khanatal92 <akhan126@insite.4cd.edu>
2024-03-15 20:20:28 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "main.hpp"
 
 using namespace std;
 
 int writeFile() {
     ofstream ofs("employee.txt");
-    if (!ofs.is_open()) {
-        cerr << "Error opening file." << endl;
-        return -1;
+    if (!ofs) {
+        cout << "File Open Error\n";
+        exit(0);
     }
 
     int numEmployees;
     cout << "Enter the number of employees: ";
     cin >> numEmployees;
+
     ofs << numEmployees << endl;
 
     for (int i = 0; i < numEmployees; ++i) {
@@ -24,34 +22,45 @@ int writeFile() {
 
         cout << "Enter the Employee ID, Name, Department, and Salary for employee " << (i + 1) << ": ";
         cin >> employeeID >> employeeName >> departmentName >> salary;
+
         ofs << employeeID << " " << employeeName << " " << departmentName << " " << salary << endl;
     }
 
     ofs.close();
-    cout << "Employee information has been written to employee.txt." << endl;
     return numEmployees;
 }
 
 int readFile() {
     ifstream ifs("employee.txt");
-    if (!ifs.is_open()) {
-        cerr << "Error opening file." << endl;
-        return -1;
+    if (!ifs) {
+        cout << "File Open Error\n";
+        exit(0);
     }
 
     int numEmployees;
     ifs >> numEmployees;
-    cout << "Number of employees: " << numEmployees << endl;
 
+    cout << endl << "Employee information in employee.txt:" << endl;
     for (int i = 0; i < numEmployees; ++i) {
         int employeeID;
         string employeeName, departmentName;
         double salary;
 
         ifs >> employeeID >> employeeName >> departmentName >> salary;
+
         cout << "Employee " << (i + 1) << ": " << employeeID << " " << employeeName << " " << departmentName << " " << salary << endl;
     }
 
     ifs.close();
     return numEmployees;
+}
+
+int main() {
+    int numEmployeesWritten = writeFile();
+    int numEmployeesRead = readFile();
+
+    cout << "Total employees written: " << numEmployeesWritten << endl;
+    cout << "Total employees read: " << numEmployeesRead << endl;
+
+    return 0;
 }
