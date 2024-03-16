6850b5f76a568de1bbcd0166346a0b9d0b83ed08
khanatal92 <akhan126@insite.4cd.edu>
2024-03-15 20:15:56 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
-
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <cstring>
+#include <string>
 #include "main.hpp"
 
 using namespace std;
 
 int writeFile() {
     ofstream ofs("employee.txt");
-    if (!ofs) {
-        cerr << "File Open Error" << endl;
+    if (!ofs.is_open()) {
+        cerr << "Error opening file." << endl;
         return -1;
     }
 
@@ -30,42 +28,30 @@ int writeFile() {
     }
 
     ofs.close();
-    cout << "Employee information has been written to employee.txt" << endl;
+    cout << "Employee information has been written to employee.txt." << endl;
     return numEmployees;
 }
 
 int readFile() {
     ifstream ifs("employee.txt");
-    if (!ifs) {
-        cerr << "File Open Error" << endl;
+    if (!ifs.is_open()) {
+        cerr << "Error opening file." << endl;
         return -1;
     }
 
     int numEmployees;
     ifs >> numEmployees;
-    cout << setw(12) << "ID" << setw(20) << "Name" << setw(20) << "Department" << setw(12) << "Salary" << endl;
-
-    int employeeID;
-    string employeeName, departmentName;
-    double salary;
-    double totalSalary = 0;
+    cout << "Number of employees: " << numEmployees << endl;
 
     for (int i = 0; i < numEmployees; ++i) {
+        int employeeID;
+        string employeeName, departmentName;
+        double salary;
+
         ifs >> employeeID >> employeeName >> departmentName >> salary;
-
-        cout << setw(12) << employeeID << setw(20) << employeeName << setw(20) << departmentName << setw(12) << salary << endl;
-
-        totalSalary += salary;
+        cout << "Employee " << (i + 1) << ": " << employeeID << " " << employeeName << " " << departmentName << " " << salary << endl;
     }
 
-    cout << setw(52) << "Total: " << setw(12) << totalSalary << " Average: " << setw(12) << fixed << setprecision(2) << (totalSalary / numEmployees) << endl;
-
     ifs.close();
-    return 0;
-}
-
-int main() {
-    writeFile();
-    readFile();
-    return 0;
+    return numEmployees;
 }
--- main.hpp
+++ main.hpp
@@ -1,6 +1,8 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#include <iostream>
+#include <fstream>
 #include <string>
 
 int writeFile();
